71cf0bc04b650855dcb73b871174b9f962ab5f51
zverok <zverok.offline@gmail.com>
2021-06-12 20:39:33 +0300

    Initial

---
+++ wikipedia_ql/fragment.py
@@ -0,0 +1,114 @@
+import copy
+import re
+import json
+import bs4
+from spacy.lang.en import English
+from bs4 import BeautifulSoup
+
+class Fragment:
+    REMOVE = [
+        '#toc',
+        '.mw-editsection',
+        'sup.reference',
+        'div.reflist',
+        'style'
+    ]
+
+    @classmethod
+    def from_json(cls, data):
+        return cls.parse(data['parse']['text']['*'])
+
+    @classmethod
+    def parse(cls, html):
+        soup = BeautifulSoup(html, 'html.parser').select('.mw-parser-output')[0]
+        for sel in cls.REMOVE:
+            for tag in soup.select(sel):
+                tag.extract()
+
+        return cls(soup)
+
+    def __init__(self, soup):
+        self.soup = soup
+        self.text = ''
+        def attach_to_text(node):
+            node.textstart = len(self.text)
+
+            if isinstance(node, bs4.element.NavigableString):
+                self.text += str(node)
+            else:
+                for child in node.children:
+                    attach_to_text(child)
+                if node.name in ['p', 'div', 'ul', 'ol', 'li', 'table', 'tbody', 'tr', 'br', 'h2', 'h3', 'h4', 'h5']:
+                    self.text += "\n"
+                elif node.name in ['td', 'th']:
+                    self.text += " "
+                else:
+                    pass
+                    # print(node.name)
+
+            node.textend = len(self.text)
+
+        for child in soup.children:
+            attach_to_text(child)
+
+        nlp = English()
+        nlp.add_pipe(nlp.create_pipe("sentencizer"))
+        doc = nlp(self.text)
+        # FIXME: Newlines are ignored this way! Sentences include tables and hNs and whatnot
+        self.sentences = [*doc.sents]
+
+    def slice(self, start, end):
+        def filter_children(node):
+            if isinstance(node, bs4.element.NavigableString):
+                s = start - node.textstart if node.textstart < start else 0
+                e = end - node.textend if node.textend > end else -1
+                text = str(node)[s:e]
+                node.replace_with(text)
+                return
+
+            for child in [*node.children]:
+                if child.textend < start or child.textstart > end:
+                    child.extract()
+                elif child.textstart < start or child.textend > end:
+                    filter_children(child)
+                else:
+                    # Entirely inside the range
+                    pass
+
+        def copy_pos(from_node, to_node):
+            to_node.textstart = from_node.textstart
+            to_node.textend = from_node.textend
+
+            if isinstance(to_node, bs4.element.Tag):
+                for from_c, to_c in zip(from_node.children, to_node.children):
+                    copy_pos(from_c, to_c)
+
+        res = copy.copy(self.soup)
+        for from_child, to_child in zip(self.soup.children, res.children):
+            copy_pos(from_child, to_child)
+
+        filter_children(res)
+
+        return Fragment(res)
+
+    def select(self, selector, *selectors):
+        fragments = Fragments(self._select(selector))
+        if selectors:
+            return fragments.select(*selectors)
+        else:
+            return fragments
+
+    def _select(self, selector):
+        return [self.slice(f, t) for f, t in selector(self)]
+
+class Fragments:
+    def __init__(self, items):
+        self.items = items
+
+    def select(self, selector, *selectors):
+        fragments = Fragments([nested for item in self.items for nested in item._select(selector)])
+
+        if selectors:
+            return fragments.select(*selectors)
+        else:
+            return fragments
---
+++ wikipedia_ql/selectors.py
@@ -0,0 +1,57 @@
+import re
+import bs4
+
+class text:
+    def __init__(self, re):
+        self.re = re
+
+    def __call__(self, page):
+        yield from (m.span(0) for m in self.re.finditer(page.text))
+
+class sentence:
+    def __init__(self, pattern):
+        self.pattern = pattern
+
+    def __call__(self, page):
+        def matches(text):
+            if isinstance(self.pattern, re.Pattern):
+                return self.pattern.search(text)
+            elif isinstance(self.pattern, str):
+                return self.pattern in text
+
+        yield from ((sent.start_char, sent.end_char) for sent in page.sentences if matches(sent.text))
+
+class section:
+    # TODO: Level as an optional filter
+    def __init__(self, text):
+        self.text = text
+
+    def __call__(self, page):
+        first = None
+        last = None
+        # FIXME: Currently only yields non-intersecting ones
+        for child in page.soup.children:
+            if first:
+                if isinstance(child, bs4.element.Tag) and child.name.startswith('h') and child.name <= first.name:
+                    yield (first.textstart, last.textend)
+                    first = None
+                    last = None
+                else:
+                    last = child
+            else:
+                if isinstance(child, bs4.element.Tag) and child.name.startswith('h') and self.text in child.string:
+                    first = child
+
+class css:
+    def __init__(self, selector):
+        self.selector = selector
+
+    def __call__(self, page):
+        yield from ((node.textstart, node.textend) for node in page.soup.select(self.selector))
+
+class all:
+    def __init__(self, *selectors):
+        self.selectors = selectors
+
+    def __call__(self, page):
+        yield from (slice for selector in self.selectors for slice in selector(page))
